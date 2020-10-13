1dded55ddeb11722be9022873a574fc1c344ff7e
Ab2kone <ab2kone@gmail.com>
2020-10-13 19:00:45 +0100

    React installation & creating diffrents folders to our application and first test

---
+++ src/constants/tweets.js
@@ -0,0 +1,27 @@
+const tweets = [{
+    avatar: 'https://pbs.twimg.com/profile_images/1234594515.png',
+    username: 'dan_abramov',
+    fullname: 'Dan Abramov',
+    content: 'Now, this is a configuration that makes sense',
+    date: '12/12/2016',
+}, {
+    avatar: 'https://pbs.twimg.com/profile_images/1234594515.png',
+    username: 'elephorm',
+    fullname: 'Elephorm',
+    content: 'Now, this is a configuration that makes sense',
+    date: '12/10/2016',
+}, {
+    avatar: 'https://pbs.twimg.com/profile_images/1234594515.png',
+    username: 'hqro',
+    fullname: 'Guillaume Larivière',
+    content: 'Now, this is a configuration that makes sense',
+    date: '12/05/2016',
+}, {
+    avatar: 'https://pbs.twimg.com/profile_images/1234594515.png',
+    username: 'reactjs_fr',
+    fullname: 'ReactJS.fr',
+    content: 'Now, this is a configuration that makes sense',
+    date: '12/02/2016',
+}];
+
+export default tweets;
